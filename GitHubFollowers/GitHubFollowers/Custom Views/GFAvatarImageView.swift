11315771766a78e8f443fd997892748535564ad1
miktaba <mtabakaev@gmail.com>
2024-09-12 14:09:51 +0200

    Implemented cache

--- GitHubFollowers/GitHubFollowers/Custom Views/GFAvatarImageView.swift
+++ GitHubFollowers/GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
-
-let placeholderImage = UIImage(named: "GFPlaceholder")
+import Foundation
 
 class GFAvatarImageView: UIImageView {
+    
+    let cache = NetworkManager.shared.cache
+    
+    let placeholderImage = UIImage(named: "GFPlaceholder")
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,5 +29,34 @@ class GFAvatarImageView: UIImageView {
         layer.cornerRadius = 10
         clipsToBounds = true
         image = placeholderImage
+    }
+    
+    
+    func downloadImage(from urlString: String) {
+        
+        let cacheKey = urlString
+        
+        if let image = cache.object(forKey: cacheKey as NSString) {
+            self.image = image
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let self = self else { return }
+            if error != nil { return }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            
+            guard let data = data else { return }
+            
+            guard let image = UIImage(data: data) else { return }
+            self.cache.setObject(image, forKey: cacheKey as NSString)
+            
+            DispatchQueue.main.async { self.image = image }
+        }
+        
+        task.resume()
     }
 }
--- GitHubFollowers/GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/GitHubFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Mikhail Tabakaev on 9/7/24.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
     let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
